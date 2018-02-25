7a7552cd66db2a8323ad56bb3e5f89d45eeef409
Ilya Minyukhin <minyukhin.ilya@gmail.com>
2018-02-25 17:46:38 +0600

    helloservlet

---
+++ src/main/java/it/sevenbits/helloservlet/SimpleHttpServlet.java
@@ -0,0 +1,18 @@
+package it.sevenbits.helloservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleHttpServlet extends HttpServlet {
+
+    protected void doGet( HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.getWriter().write(
+                "<html><body>Hello, world!</body></html>");
+    }
+}
